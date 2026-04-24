da578f343cfdba6b3efcfd87d1f4a2b50083ae06
Thulani <theshikathulee@gmail.com>
2026-04-24 09:07:27 +0530

    Initial Commit

---
+++ LearnTrack/LearnTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTrackApp.swift
+//  LearnTrack
+//
+//  Created by COBSCCOMP242P-028 on 2026-04-24.
+//
+
+import SwiftUI
+
+@main
+struct LearnTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
